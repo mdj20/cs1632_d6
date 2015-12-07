797e86b41fee439e0d8ca9dbe227d933fc534037
Matthew <mdj20@pitt.edu>
2015-12-07 03:39:51 -0500

    World.java work

--- World.java
+++ World.java
@@ -1,15 +1,18 @@
 import java.util.ArrayList;
+import java.util.HashSet;
 
 public class World {
 
 	private Room _currentLocation;
-	private ArrayList< Item > _inventory;
+	private HashSet< Item > _inventory;
 	private ArrayList< Room > _world;
 	
 	private ArrayList< Door > _currentDoors;
 	private ArrayList< Chest > _currentChests;
 	private ArrayList< Container > _currentContainers;
 	private ArrayList< Attribute > _currentAttributes;
+	
+	private StringBuilder _sb;
 	 
 	
 	World( ArrayList<Room> world, Room start ){
@@ -17,6 +20,8 @@ public class World {
 			_world = world;
 			_currentLocation = start;
 			setUpRoom();
+			_inventory = new HashSet<Item>();
+			_sb = new StringBuilder();
 			
 	}
 	
@@ -30,13 +35,25 @@ public class World {
 		
 	}
 	
-	public boolean move( int  ){
-		boolean value = false;
+	// attmpts to move in the direction passed in.
+	public String move( int d ){
 		
+		String value = "You can't go there";
+		
+		
+			// checks if door in that direction exists
 			for (Door door : _currentDoors){
-			
-				if( door.direction() == d ){
+				
+				// if door is found, create response string for value
+				if( door.direction(_currentLocation) == d ){
+					_currentLocation = door.enter(_currentLocation);
+					setUpRoom();
 					
+					_sb = new StringBuilder();
+					_sb.append("You enter ");
+					_sb.append(_currentLocation.name());
+					value = _sb.toString();
+					break; // break for
 				}
 					
 			}
@@ -44,7 +61,42 @@ public class World {
 		return value;
 	}
 	
+	// returns string of name of item passed through
 	public String look(String target){
+		
+		String value = "You can't look at that.";
+		
+		
+		// search for target match
+		ArrayList<Attribute> nonContainers = new ArrayList<Attribute>();
+		nonContainers.addAll(_currentChests);
+		nonContainers.addAll(_currentDoors);
+		nonContainers.addAll(_currentAttributes);
+		
+		for (Attribute a : nonContainers){
+		
+			if(target.equals(a.name())){
+				value = a.name();
+				break;
+			}
+		
+		}
+		
+		for (Container c : _currentContainers){
+			
+			if (target.equals(c.name())){
+				value = c.name();
+				
+				if (c.contains()){
+					_inventory.add(c.get());
+				}
+				
+			}
+			
+		}
+		
+		
+		return value;
 		
 	}
 	
